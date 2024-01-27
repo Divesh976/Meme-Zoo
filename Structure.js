9924ebb1dec92b0a15498672108307c3219a8dae
Divesh976 <143112709+Divesh976@users.noreply.github.com>
2024-01-27 18:58:24 +0530

    Add files via upload

---
+++ Structure.js
@@ -0,0 +1,6 @@
+const HEADING = document.getElementById("Heading")
+const SURPRISE = document.getElementById("Surprise")
+
+SURPRISE.onclick = function(){
+    HEADING.textContent = "I hope you have fantastic time in this haven!"
+}
